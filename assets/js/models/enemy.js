caade4c6240fa961081aaa3a80afe1d14953d659
Pablo Mateos <pmateos805@gmail.com>
2021-12-08 21:36:21 +0100

    Enemy in progress

---
+++ assets/js/models/enemy.js
@@ -0,0 +1,110 @@
+class Enemy {
+    constructor(ctx, x, y){
+        this.ctx = ctx
+        this.x = x
+        this.y = y 
+
+        this.width = 60*2
+        this.height = 120*2
+
+        this.speed = 6
+        this.vx = 0
+        this.vy = 0
+
+        this.health = 100
+
+        this.img = new Image()
+        this.img.src = '#'
+        this.img.isReady = false
+        this.img.onload = () => {
+            this.img.isReady = true
+        }
+
+        this.horizontalFrames = 4
+        this.verticalFrames = 1
+
+        this.xFrame = 0 
+        this.yFrame = 0 
+
+        this.ticks = 0
+
+        this.movements = {
+            up: false,
+            down: false,
+            left: false,
+            right: false
+        }
+    }
+
+    draw(){
+        if(this.img.isReady){
+            this.ctx.drawImage(
+                    this.img,
+                    (this.img.width * this.xFrame) / this.horizontalFrames,
+                    (this.img.height * this.yFrame) / this.verticalFrames,
+                    this.img.width / this.horizontalFrames,
+                    this.img.height * this.verticalFrames,
+                    this.x,
+                    this.y,
+                    this.width,
+                    this.height
+            )
+            this.ticks++
+
+        }
+    }
+
+    setupListeners(event) {
+        const status = event.type === 'keydown'
+    
+        switch(event.keyCode) {
+          case KEY_UP:
+            this.movements.up = status
+            break
+          case KEY_DOWN:
+            this.movements.down = status
+            break
+          case KEY_RIGHT:
+            this.movements.right = status
+            break
+          case KEY_LEFT:
+            this.movements.left = status
+            break
+          default:
+            break
+        }
+      }
+
+      move(){
+        if (!this.movements.right && !this.movements.left) {
+          this.vx = 0
+        }
+        if (!this.movements.up && !this.movements.down) {
+          this.vy = 0
+        }
+    
+        if (this.movements.right) {
+          this.vx = -this.speed
+        }
+        if (this.movements.left) {
+          this.vx = this.speed
+        }
+        
+        if (this.movements.up) {
+            this.vy = this.speed
+        }
+        if (this.movements.down) {
+            this.vy = -this.speed
+        }
+
+        this.x += this.vx
+        this.y += this.vy
+
+        if(this.ticks % 10 === 0) {
+            this.xFrame++
+            if(this.xFrame >= (this.horizontalFrames)){
+                this.xFrame = 0
+            }
+        }
+    }
+}
--- assets/js/models/game.js
+++ assets/js/models/game.js
@@ -5,7 +5,13 @@ class Game {
         this.background = new Background(ctx)
         this.player = new Player(ctx)
         this.keyPiece = [
-            new keyPiece(ctx, 750, 700),
+            new keyPiece(ctx, 2720, 750),
+            new keyPiece(ctx, 999, 3800),
+            new keyPiece(ctx, 2800, 2000)
+        ]
+        this.enemy = [
+            new Enemy(ctx, 150, 150),
+            new Enemy(ctx, 250, 350)
         ]
 
         this.intervalId = undefined
@@ -33,25 +39,28 @@ class Game {
         this.background.move()
         this.player.move() 
         this.moveKeys()
+        this.enemy.forEach(enemy => enemy.move())
     }
 
     moveKeys() {
         this.keyPiece.forEach(keyPiece => keyPiece.move())
     }
 
+    // pendiente de pasar a otro metodo
     drawKeyPieces() {
         this.ctx.save()
         this.ctx.fillStyle= "rgba(255, 255, 255, 0.8)"
-        this.ctx.fillRect(20, 15, 116, 38)
+        this.ctx.fillRect(20, 15, 250, 60)
         this.ctx.restore()
-        this.ctx.font = 'bold 20px monospace'
-        this.ctx.fillText(`Keys ${this.keysTaken}/${this.keyPiece.length}`, 30, 40)
+        this.ctx.font = 'bold 40px monospace'
+        this.ctx.fillText(`Keys ${this.keysTaken}/ 3`, 40, 60)
     }
 
     draw() {
         this.background.draw()
         this.player.draw()
         this.keyPiece.forEach(keyPiece => keyPiece.draw())
+        this.enemy.forEach(enemy => enemy.draw())
         this.drawKeyPieces()
     }
 
@@ -59,6 +68,7 @@ class Game {
         this.background.setupListeners(event)
         this.player.setupListeners(event)
         this.keyPiece.forEach(key => key.setupListeners(event))
+        this.enemy.forEach(enemy => enemy.setupListeners(event))
     }
 
     checkCollision(){
