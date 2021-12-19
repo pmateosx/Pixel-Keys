c67e231549764ffa04fd6f8330fc1a0c9eeb9653
Pablo Mateos <pmateos805@gmail.com>
2021-12-19 15:00:59 +0100

    text and intro

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,10 +5,19 @@ window.addEventListener('load', () => {
     const game = new Game(ctx)
 
     const startButton = document.getElementById('start-button')
+    const gameLogo = document.getElementById('game-logo')
+    const introText = document.getElementById('introText')
+    const movements = document.getElementById('movements')
+    movements.style.display = 'none'
+
 
     startButton.onclick = () => {
         game.start()
         startButton.style.display = 'none'
+        gameLogo.style.display = 'none'
+        introText.style.display = 'none'
+        movements.style.display = 'initial'
+        
         // que vuelva a aparecer el boton una vez muera
     }
 
